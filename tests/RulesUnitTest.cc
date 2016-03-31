4d9f97199fdb7a1507fe8dec104bd19da1ad3f4d
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2016-03-31 23:01:21 +0200

    tests: fix gtest library url

--- tests/RulesUnitTest.cc
+++ tests/RulesUnitTest.cc
@@ -12,7 +12,7 @@
 #include <vector>
 
 // Google test tools
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 // project-local
 #include "utils.hh"
@@ -90,12 +90,12 @@ TEST( NumberRuleTest, one_color )
     InSequence dummy;
     const uint8_t lLinesCount( 1 );
     NumberRule::Ptr lRule( new NumberRule( RED, lLinesCount ) );
-    
+
     std::vector< std::string > lLines;
     std::string lBaseLine( "This is a Line" );
-    
+
     typedef uint8_t testedType;
-    for ( testedType lI( 0 ) 
+    for ( testedType lI( 0 )
             ; lI < std::numeric_limits< testedType >::max() / 20
             ; ++lI )
     {
@@ -104,7 +104,7 @@ TEST( NumberRuleTest, one_color )
         lLines.push_back( lStream.str() );
     }
 
-    for_each( lLines.begin(), lLines.end(), [=]( std::string aLine ) 
+    for_each( lLines.begin(), lLines.end(), [=]( std::string aLine )
            { uint32_t lI( 0 )
             ; MockIntermediateResult lIResult;
             ; EXPECT_CALL( lIResult, putMarker( 0, RED ) )
@@ -126,12 +126,12 @@ TEST( NumberRuleTest, two_colors )
     const uint8_t lLinesCount( 3 );
     InSequence dummy;
     NumberRule::Ptr lRule( new NumberRule( RED, lLinesCount ) );
-    
+
     std::vector< std::string > lLines;
     std::string lBaseLine( "This is a Line" );
-    
+
     typedef uint8_t testedType;
-    for ( testedType lI( 0 ) 
+    for ( testedType lI( 0 )
             ; lI < std::numeric_limits< testedType >::max() / 20
             ; ++lI )
     {
@@ -140,7 +140,7 @@ TEST( NumberRuleTest, two_colors )
         lLines.push_back( lStream.str() );
     }
 
-    for_each( lLines.begin(), lLines.end(), [=]( std::string aLine ) 
+    for_each( lLines.begin(), lLines.end(), [=]( std::string aLine )
            { uint32_t lI( 0 )
             ; MockIntermediateResult lIResult;
             ; EXPECT_CALL( lIResult, putMarker( 0, RED ) )
